3a2959601194036d70d3dad1c3babef29f6c7458
pauljouet <licornedu92i@gmail.com>
2020-11-26 11:03:07 +0100

    created main, created env, made sure everything works fine

---
+++ main.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 26 10:39:26 2020
+
+@author: Paul Jouet
+"""
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
